06711e9e00d2cd9f6ea06f0d0a028b7af35e5d12
AppleCEO <appleceo@kakao.com>
2022-08-01 21:41:53 +0900

    카드 뒤집기 구현

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
+  
+  @IBAction func touchCard(_ sender: UIButton) {
+    flipCard(withEmoji: "👻", on: sender)
   }
-
-
+  
+  func flipCard(withEmoji emoji: String, on button: UIButton) {
+    if button.currentTitle == emoji {
+      button.setTitle("", for: .normal)
+      button.backgroundColor = .orange
+    } else {
+      button.setTitle(emoji, for: .normal)
+      button.backgroundColor = .white
+    }
+  }
 }
 
